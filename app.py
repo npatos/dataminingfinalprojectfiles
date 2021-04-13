2a326be4e587904646c9dcef29580b6af5ba3489
npatos <35760370+npatos@users.noreply.github.com>
2021-04-13 03:09:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
-from flask import Flask,render_template,request 
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity,linear_kernel    
-from flask import Flask, render_template, request, jsonify
-
-import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity,linear_kernel      
 from flask import Flask, render_template, request, jsonify
 import numpy as np
 import tensorflow as tf
@@ -19,7 +13,7 @@ app = Flask(__name__)
 
 def load_data(data):
     df= pd.read_csv(data, sep= ';', error_bad_lines= False, encoding= 'latin-1')
-    df=df.head(500)
+    df=df.head(100)
     return df
 df=load_data('https://raw.githubusercontent.com/tttgm/fellowshipai/master/book_crossing_dataset/BX-Books.csv')
 
@@ -35,7 +29,7 @@ def login_page():
   authors_rating=df_rating['book_author']
   years_rating=df_rating['year_of_publication']
   images_rating = df_rating['image_url_l']
-  # COLLABORATIVE
+    
   df= pd.read_csv('https://raw.githubusercontent.com/npatos/dataminingfinalprojectdata/main/Finalcollab.csv')
   coll_titles = df['book_title']
   coll_authors=df['book_author']
@@ -56,10 +50,10 @@ def book_collaborative_recommend(coll_titles):
   user_id=user_id.iloc[0]
   user_r = user_id
   b_id =list(combine_book_rating_data.user.unique())
-  book_arr = np.array(b_id) #get all book IDs
+  book_arr = np.array(b_id) 
   user = np.array([user_r for i in range(len(b_id))])
   pred = model.predict([book_arr, user])
-  pred = pred.reshape(-1) #reshape to single dimension
+  pred = pred.reshape(-1)
   pred_ids = (-pred).argsort()[0:10]
   top10 = combine_book_rating_data.iloc[pred_ids]
   f=['book_title','book_author','year_of_publication','image_url_l']
